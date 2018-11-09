fd3d83fb072c95ee0ab93f2ecacd363141f2cf9c
frederic wagner <frederic.wagner@imag.fr>
2018-11-09 07:48:46 +0100

    clique: wip

--- examples/clique.rs
+++ examples/clique.rs
@@ -8,57 +8,29 @@ use std::collections::HashSet;
 
 type PointIndex = usize;
 
-
-fn compute_dominating_points<I: Iterator<Item = Point>>(
-    points: I,
-    dominating_points: &mut HashSet<PointIndex>,
-) {
-    let mut points:Vec<Point> = points.collect();
-    points.sort_unstable();
-    let mut best_value = std::f64::NEG_INFINITY;
-    let mut not_ok = HashSet::new();
-    for (index, point) in points.iter().enumerate().rev() {
-        let value = point.y - point.x;
-        if value > best_value {
-            best_value = value;
-        } else {
-            not_ok.insert(index);
-        }
-    }
-    let mut best_value = std::f64::NEG_INFINITY;
-    // TODO: check i can rev in O(1)
-    for (index, point) in points.iter().enumerate() {
-        let value = point.y + point.x;
-        if value > best_value {
-            best_value = value;
-        } else {
-            if not_ok.contains(&index) {
-                dominating_points.insert(index);
-            }
+fn update_edge<I: Iterator<Item=PointIndex>, F: Fn(PointIndex)->f64>(edge_points: &mut HashSet<PointIndex>, points_indices: I, score_function : F) {
+    let mut best_score = std::f64::NEG_INFINITY;
+    for index in points_indices {
+        let score = score_function(index);
+        if score > best_score {
+            best_score = score;
+            edge_points.insert(index);
         }
     }
 }
 
 fn main() {
     let points: Vec<Point> = repeat_call(|| Point::new(random(), random()))
-        .take(10)
+        .take(100)
         .collect();
-    let mut dominating_points = HashSet::new();
-    // TODO: sort only twice
-    compute_dominating_points(points.iter().cloned(), &mut dominating_points);
-    compute_dominating_points(
-        points.iter().map(|p| Point::new(p.x, -p.y)),
-        &mut dominating_points,
-    );
-    compute_dominating_points(
-        points.iter().map(|p| Point::new(p.y, p.x)),
-        &mut dominating_points,
-    );
-    compute_dominating_points(
-        points.iter().map(|p| Point::new(p.y, -p.x)),
-        &mut dominating_points,
-    );
-    let dpoints: Vec<_> = dominating_points.iter().map(|i| points[*i]).collect();
+    let mut horizontal_points:Vec<PointIndex> = (0..points.len()).collect();
+    horizontal_points.sort_unstable_by(|&i1, &i2| points[i1].cmp(&points[i2]));
+
+    let mut edge_points = HashSet::new();
+    update_edge(&mut edge_points, horizontal_points.iter().cloned(), |i| points[i].x + points[i].y);
+    update_edge(&mut edge_points, horizontal_points.iter().rev().cloned(), |i| -points[i].x + points[i].y);
+
+    let dpoints: Vec<_> = edge_points.iter().map(|i| points[*i]).collect();
     tycat!(points, dpoints);
     println!(
         "we now have {} points instead of {} !",
